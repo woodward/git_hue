910cb137dd197e9e2373d531fa7c4b39ebdb6bbe
Greg Woodward <woodward@users.noreply.github.com>
2023-08-06 10:57:21 -0700

    Add test values for various application env settings

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,14 +1,16 @@
 import Config
 
-config :git_hue,
-  hue_unique_identifier: System.get_env("HUE_UNIQUE_IDENTIFIER"),
-  # Note that :hue_bridge_ip_address can be nil if using Philip's NUPNP discovery service (the default behavior)
-  hue_bridge_ip_address: System.get_env("HUE_BRIDGE_IP_ADDRESS"),
-  hue_light_name: System.get_env("HUE_LIGHT_NAME"),
-  #
-  github_polling_interval_sec: System.get_env("GITHUB_POLLING_INTERVAL_SEC") |> String.to_integer(),
-  github_personal_access_token: System.get_env("GITHUB_PERSONAL_ACCESS_TOKEN"),
-  github_owner_repo: System.get_env("GITHUB_OWNER_REPO"),
-  github_ci_job_name: System.get_env("GITHUB_CI_JOB_NAME"),
-  github_branch_name: System.get_env("GITHUB_BRANCH_NAME", nil),
-  start_github_monitor_automatically?: config_env() != :test
+if config_env() != :test do
+  config :git_hue,
+    hue_unique_identifier: System.get_env("HUE_UNIQUE_IDENTIFIER"),
+    # Note that :hue_bridge_ip_address can be nil if using Philip's NUPNP discovery service (the default behavior)
+    hue_bridge_ip_address: System.get_env("HUE_BRIDGE_IP_ADDRESS"),
+    hue_light_name: System.get_env("HUE_LIGHT_NAME"),
+    #
+    github_polling_interval_sec: System.get_env("GITHUB_POLLING_INTERVAL_SEC") |> String.to_integer(),
+    github_personal_access_token: System.get_env("GITHUB_PERSONAL_ACCESS_TOKEN"),
+    github_owner_repo: System.get_env("GITHUB_OWNER_REPO"),
+    github_ci_job_name: System.get_env("GITHUB_CI_JOB_NAME"),
+    github_branch_name: System.get_env("GITHUB_BRANCH_NAME", nil),
+    start_github_monitor_automatically?: true
+end
--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,16 @@ import Config
 
 # disable SSL for tests
 config :hue_sdk, ssl: false
+
+config :git_hue,
+  hue_unique_identifier: "test-hue-identifier",
+  # Note that :hue_bridge_ip_address can be nil if using Philip's NUPNP discovery service (the default behavior)
+  hue_bridge_ip_address: nil,
+  hue_light_name: "github",
+  #
+  github_polling_interval_sec: 30,
+  github_personal_access_token: "github-personal-access-token",
+  github_owner_repo: "my-owner/my-repo",
+  github_ci_job_name: "CI Job",
+  github_branch_name: "main",
+  start_github_monitor_automatically?: false
