e5bddd9735b8096ddb105f6f42ebeaeb778b3510
Greg Woodward <woodward@users.noreply.github.com>
2023-08-07 10:18:01 -0700

    Backfill some tests for when the GitHubMonitor stops due to error conditions

--- test/git_hue/git_hub_monitor_test.exs
+++ test/git_hue/git_hub_monitor_test.exs
@@ -32,4 +32,57 @@ defmodule GitHue.GitHubMonitorTest do
       assert state == %{bridge: authenticated_bridge(), light_id: "2", color: :yellow}
     end
   end
+
+  describe "GitHubMonitor stops if cannot initialize the Hue bridge" do
+    setup do
+      patch(HueSDK.Discovery, :discover, fn _type -> {:nupnp, [bridge()]} end)
+
+      patch(HueSDK.Bridge, :authenticate, fn bridge, _unique_identifier ->
+        # Note that the username is not set here, which means that authentication has failed:
+        bridge
+      end)
+
+      :ok
+    end
+
+    @tag capture_log: true
+    test "starts the genserver and then stops" do
+      {:ok, github_monitor_pid} = start_supervised(GitHubMonitor)
+
+      # Do something instead of Process.sleep() here?
+      Process.sleep(10)
+
+      assert Process.alive?(github_monitor_pid) == false
+    end
+  end
+
+  describe "GitHubMonitor stops if cannot get the workflow runs from GitHub" do
+    setup do
+      patch(HueSDK.Discovery, :discover, fn _type -> {:nupnp, [bridge()]} end)
+      patch(HueSDK.Bridge, :authenticate, fn bridge, _unique_identifier -> Map.put(bridge, :username, username()) end)
+      patch(HueSDK.API.Lights, :get_all_lights, fn _bridge -> {:ok, lights()} end)
+
+      patch(Req, :get!, fn _req, _params ->
+        %Req.Response{
+          private: %{},
+          status: 404,
+          headers: [
+            {"server", "GitHub.com"}
+          ]
+        }
+      end)
+
+      :ok
+    end
+
+    @tag capture_log: true
+    test "starts the genserver and then stops" do
+      {:ok, github_monitor_pid} = start_supervised(GitHubMonitor)
+
+      # Do something instead of Process.sleep() here?
+      Process.sleep(10)
+
+      assert Process.alive?(github_monitor_pid) == false
+    end
+  end
 end
